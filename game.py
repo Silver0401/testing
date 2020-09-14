0658b3e4aa394d1b829a0903256b9bc2c33eea88
VictorPonce27 <66763627+VictorPonce27@users.noreply.github.com>
2020-09-14 11:53:33 -0500

    Update game.py
    Funcion de area

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
+
+def calc_area(input1, input2): 
+    return input1*input2; 
+
+
 val1 = float(input("dame el primer valor"))
 val2 = float(input("dame el segundo valor"))
 perimetro=((2*val1)+(2*val2))
 
+
+val2 = float(input("dame el segundo valor"))
 
