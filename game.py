21830a496c23d89f402ff47827c71d220bd291a2
Ismael Muñoz <Prosilver05@gmail.com>
2020-09-14 11:59:07 -0500

    ya todo jala

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 
 def calc_area(input1, input2): 
-    return input1*input2; 
+    return "Area: " + str(input1*input2); 
 
+def calc_per(coso1, coso2):
+    return "Perimetro: " + str((2*coso1)+(2*coso2))
 
-val1 = float(input("dame el primer valor"))
-val2 = float(input("dame el segundo valor"))
-perimetro=((2*val1)+(2*val2))
+val1 = float(input("dame el primer valor: "))
+val2 = float(input("dame el segundo valor: "))
 
+print(calc_area(val1,val2))
+print(calc_per(val1,val2))
 
-val2 = float(input("dame el segundo valor"))
 
